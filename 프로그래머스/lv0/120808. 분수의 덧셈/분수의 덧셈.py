f2b7a96f71781cd634a8c4c63007b93a977ee09b
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-02-18 00:45:19 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,14 @@
-from math import gcd #파이썬 최대공약수 내장함수 호출 
-def solution(denum1, num1, denum2, num2): 
-    m = num1 * num2 // gcd(num1, num2) #최소공배수(분모)  *최대공약수 gcd(num1, num2)
-    
-    sx1 = int(m / num1)
-    sx2 = int(m / num2)
-    
-    s = (sx1*denum1) + (sx2*denum2)
+from math import gcd #파이썬 최대공약수 내장함수 호출
+#from math import lcm 파이썬 최소공배수 내장함수 호출 *파이썬 버전 3.9上 / 프로그래머스 파이썬 버전 3.8.5
 
-    ml = gcd(s, m)
-
-    return int(s/ml),int(m/ml)
+def solution(num1, den1, num2, den2):
+    den = den1 * den2 // gcd(den1, den2) #최소공배수(분모) *최대공약수 gcd(den1,den2)
+    
+    den_1 = int(den / den1) #최소공배수로 첫번째 분모와 나눔
+    den_2 = int(den / den2) #최소공배수로 두번째 분모와 나눔
+    
+    num = (den_1*num1) + (den_2*num2) #분자에 나눈 값을 곱해줌
+    
+    ml = gcd(num, den) #값이 약분될 경우를 위한 대비용 최대공약수
+    
+    return int(num/ml), int(den/ml)
