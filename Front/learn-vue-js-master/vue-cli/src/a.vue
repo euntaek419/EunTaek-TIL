0f633b7baf5453d0dc39fe4c0b7fd951552099bf
euntaek419 <cet4713@gmail.com>
2023-02-17 22:59:33 +0900

    2208~2302_Vue.js_study

---
+++ Front/learn-vue-js-master/vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+<div>header</div>
+</template>
+
+<script>
+export default {
+    //Javascript - 인스턴스 옵션
+    methods: {
+        addNum:function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+/* css */
+</style>
