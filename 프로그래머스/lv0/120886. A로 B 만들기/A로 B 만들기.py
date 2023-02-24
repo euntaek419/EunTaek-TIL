c06a334320dd8c9f15805fa40d9bd6defbb59a33
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-02-24 23:10:52 +0900

    [level 0] Title: A로 B 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,9 @@
+def solution(before, after):
+    
+    before = ''.join(sorted(before))
+    after = ''.join(sorted(after))
+    
+    if(before == after):
+        return 1
+    else :
+        return 0
