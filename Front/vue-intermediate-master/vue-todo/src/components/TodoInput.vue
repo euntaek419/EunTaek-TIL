90f6289b8d2c43bc2a5eafd376b9e690334dd3bd
euntaek419 <cet4713@gmail.com>
2023-03-06 07:26:18 +0900

    2023/03/06_Vue

--- Front/vue-intermediate-master/vue-todo/src/components/TodoInput.vue
+++ Front/vue-intermediate-master/vue-todo/src/components/TodoInput.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
+  <div class="inputBox shadow">
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -10,11 +14,49 @@ export default {
     return {
       newTodoItem: ""
     }
+  },
+  methods: {
+    addTodo: function() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = '';
+    }
   }
-
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+
+  display: flex;
+  align-items: center;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+
+  height: 100%;
+  width: calc(100% - 3rem);
+  text-align: center;
+}
+.addContainer {
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color:white;
+  vertical-align:middle;
+}
 
 </style>
