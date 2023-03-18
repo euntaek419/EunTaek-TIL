608489d78e758d7a14ff149d7108cbc8a89f74bd
euntaek419 <cet4713@gmail.com>
2023-03-18 23:34:56 +0900

    2023/03/18_Vue

---
+++ Front/LV.3_vue-advanced-master/vue-news/src/app.js
@@ -0,0 +1,15 @@
+// var a = 10;
+// var a = 10
+
+// import AppHeader from './components/AppHeader'
+// import Vue from 'vue';
+// import { component } from 'vue/types/umd';
+
+// if( a == 10 ) {
+//     console.log("10이다");
+// }
+
+// if (a == 1 ) console.log('10이다') b() c()
+
+
+// component
