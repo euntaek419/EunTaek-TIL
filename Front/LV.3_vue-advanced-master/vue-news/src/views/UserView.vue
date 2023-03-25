286c4b59b7c97611f6c87ba7295d087947b3cfe7
euntaek419 <cet4713@gmail.com>
2023-03-26 00:13:51 +0900

    2023/03/26_라우터_Params를_이용한_User_상세_페이지_구현

--- Front/LV.3_vue-advanced-master/vue-news/src/views/UserView.vue
+++ Front/LV.3_vue-advanced-master/vue-news/src/views/UserView.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <p>name : {{ this.$store.state.user.id }}</p>
-    <p>karma : {{ this.$store.state.user.karma }}</p>
-    <p>created : {{ this.$store.state.user.created }}</p>
+    <p>name : {{ userInfo.id }}</p>
+    <p>karma : {{ userInfo.karma }}</p>
+    <p>created : {{ userInfo.created }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed:{
+    userInfo() {
+      return this.$sotre.state.user;
+    }
+  },
   created() {
     const userName = this.$route.params.id;
     // axios.get(`https://api.hnpwa.com/v0/user/${this.$route.params.id}.json`);
     this.$store.dispatch('FETCH_USER', userName);
-  }
+  },
 }
 </script>
 
