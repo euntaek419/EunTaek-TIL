551044f037d493cc823423e9a68c8a7029056fa6
euntaek419 <cet4713@gmail.com>
2023-04-19 17:24:38 +0900

    2023/04/19_학습_노트_데이터_수정

--- Front/LV.4_vue-endgame/vue-til/src/components/posts/PostListItem.vue
+++ Front/LV.4_vue-endgame/vue-til/src/components/posts/PostListItem.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="post-time">
       {{ postItem.createdAt }}
-      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-create" @click="routeEditPage"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
@@ -16,6 +16,7 @@
 
 <script>
 import { deletePost } from '@/api/posts';
+
 export default {
   props: {
     postItem: {
@@ -25,10 +26,15 @@ export default {
   },
   methods: {
     async deleteItem() {
-      if (confirm('you want to delete it')) {
+      if (confirm('You want to delete it?')) {
         await deletePost(this.postItem._id);
         this.$emit('refresh');
       }
+      // console.log('deleted');
+    },
+    routerEditPage() {
+      const id = this.postItem._id;
+      this.$router.push(`/post/${id}`);
     },
   },
 };
