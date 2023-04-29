a61eb8535c4025f168c0035f478b7d255bbad1f1
euntaek419 <cet4713@gmail.com>
2023-04-30 03:46:45 +0900

    2023/04/30_뷰인스타그램_생성&레이아웃

---
+++ Front/vue3/vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    컨테이너 입니다.
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  components: {
+    Post,
+  }
+}
+</script>
+
+<style>
+
+</style>
