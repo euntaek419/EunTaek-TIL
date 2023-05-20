25199408a8b7d39afa5cb295c348ff195c9640ee
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-05-20 23:39:05 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    
+    counter = 0
+    
+    if(len(arr) <= 1):
+        return [-1]
+    
+    else:
+        arr.remove(min(arr))
+                
+    
+    
+    
+    
+    
+    return arr
