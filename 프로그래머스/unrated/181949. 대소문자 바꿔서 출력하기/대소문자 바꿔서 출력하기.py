39b3b22be9405183d75b2eb107dcf19ede92941b
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-08-01 04:26:15 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 10.98 ms, Memory: 7.29 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -0,0 +1,10 @@
+str = input()
+str = list(str)
+for i in range(0, len(str)):
+    if(str[i].isupper() == True):
+        str[i] = str[i].lower()
+    else:
+        str[i] = str[i].upper()
+        
+str = ''.join(s for s in str)
+print(str)
