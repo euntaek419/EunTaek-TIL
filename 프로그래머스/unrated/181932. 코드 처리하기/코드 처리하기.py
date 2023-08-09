16eb1579c9e2ce8c669a07eb43bf02365349a517
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-08-09 21:48:04 +0900

    [unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,25 @@
+def solution(code):
+    code = list(code)
+    mode = 0
+    ret = []
+    
+    for i in range(0, len(code)):
+        if(mode == 0):
+            if(code[i] != '1' and i % 2 == 0):
+                ret.append(code[i])
+            if(code[i] == '1'):
+                mode = 1
+        else:
+            if(code[i] != '1' and i % 2 != 0):
+                ret.append(code[i])
+            if(code[i] == '1'):
+                mode = 0
+    
+    if(len(ret) > 0):
+        return ''.join(ret)
+    else:
+        return "EMPTY"
+    
+        
+            
+    return
