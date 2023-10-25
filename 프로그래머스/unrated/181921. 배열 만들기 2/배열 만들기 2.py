9b936e212ae2f203bc4480da3fe2c1b78583365d
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-10-25 21:15:23 +0900

    [level unrated] Title: 배열 만들기 2, Time: 11.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,17 @@
+def solution(l, r):
+    result = []
+    for i in range(l, r+1):
+        if(i % 5 == 0): # 5의 배수이면, 다음 작업
+            if('1' not in list(str(i))):
+                if('2' not in list(str(i))):
+                    if('3' not in list(str(i))):
+                        if('4' not in list(str(i))):
+                            if('6' not in list(str(i))):
+                                if('7' not in list(str(i))):
+                                    if('8' not in list(str(i))):
+                                        if('9' not in list(str(i))):
+                                            result.append(i)
+    print(result)
+    if(result == [] ):
+        return [-1]
+    return result
