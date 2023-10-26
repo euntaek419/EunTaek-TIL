670f228d7ea81945b8623292150b5b88874940ec
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-10-26 23:31:51 +0900

    [level unrated] Title: 카운트 업, Time: 0.01 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
@@ -1,5 +1,6 @@
-def solution(start, end):
-    answer = []
-    for i in range(start, end+1):
-        answer.append(i)
-    return answer
+def solution(start_num, end_num):
+    result = []
+    for i in range(start_num, end_num+1):
+        result.append(i)
+    
+    return result
