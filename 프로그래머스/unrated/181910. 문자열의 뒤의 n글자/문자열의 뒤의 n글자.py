274819666674f0611f35dc234f81e293de545727
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-11-12 21:47:33 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.py
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.py
@@ -1,10 +1,2 @@
 def solution(my_string, n):
-    my_string = list(reversed(my_string))
-    
-    my_string = my_string[:n]
-    
-    my_string = reversed(my_string)
-    
-    my_string = ''.join(my_string)
-    
-    return my_string
+    return my_string[-n:]
