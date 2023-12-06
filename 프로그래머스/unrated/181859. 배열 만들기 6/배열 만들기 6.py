5708a0b59fadd4f561857a57783d0011891ba42c
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-12-07 01:59:36 +0900

    [level unrated] Title: 배열 만들기 6, Time: 28.42 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    i = 0
+    
+    stk = []
+    
+    while (i < len(arr)):
+        if(stk == []):
+            stk.append(arr[i])
+            i += 1
+        elif(stk != [] and stk[-1] == arr[i]):
+            stk.pop()
+            i += 1
+        elif(stk != [] and stk[-1] != arr[i]):
+            stk.append(arr[i])
+            i += 1
+    
+    if(stk == []):
+        return [-1]
+    else:
+        return stk
