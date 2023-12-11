1574a2640b1aa392cbec6ddb0f2e1e8263629ba4
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2023-12-11 19:21:07 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,15 +1,2 @@
 def solution(date1, date2):
-    
-    date1[0] = date1[0] * 10000
-    date2[0] = date2[0] * 10000
-    
-    date1[1] = date1[1] * 100
-    date2[1] = date2[1] * 100
-    
-    print(sum(date1))
-    print(sum(date2))
-    
-    if( sum(date1) >= sum(date2) ):
-        return 0
-    else:
-        return 1
+    return int(date1 < date2)
