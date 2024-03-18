2b64898f73d39b45da9b476e551456a7096a4b2b
cet4713@gmail.com <cet4713@gmail.com>
2024-03-18 21:55:45 +0900

    Lifecycle API / watch API

--- Front/vue3-intermediate/vue3-todo/src/App.vue
+++ Front/vue3-intermediate/vue3-todo/src/App.vue
@@ -8,39 +8,21 @@
 import TodoHeader from '@/components/TodoHeader.vue';
 import TodoInput from '@/components/TodoInput.vue';
 import TodoList from '@/components/TodoList.vue';
-import { ref, onBeforeMount } from 'vue'
+import {useTodo} from './hooks/useTodo'
+import { onBeforeMount } from 'vue'
 
 export default {
   components: {
     TodoHeader, TodoInput, TodoList
   },
   setup(){
-    //data
-    const todoItems = ref([])
 
-    //methods
-    function fetchTodos() {
-      const result = [];
-      for (let i = 0; i < localStorage.length; i++) {
-        const todoItem = localStorage.key(i)
-        result.push(todoItem);
-      }
-      return result;
-    }
+    const { todoItems, addTodoItem, removeTodoItem, fetchTodos } = useTodo()
 
+    //라이프사이클 API
     onBeforeMount(() => {
-      todoItems.value = fetchTodos();
+        todoItems.value = fetchTodos();
     })
-    
-    function addTodoItem(todo) {
-      todoItems.value.push(todo)
-      localStorage.setItem(todo, todo)
-    }
-
-    function removeTodoItem(item, index) {
-      todoItems.value.splice(index, 1)
-      localStorage.removeItem(item)
-    }
 
     return { todoItems, addTodoItem, removeTodoItem }
   },
