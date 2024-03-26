57ced67d60382668b0cfbde31b32c36692addcaf
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2024-03-26 19:44:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
-def solution(nums):
-    nums.sort()
-    count = 1
-    
-    for i in range(0, len(nums)):
-        if(nums[i:i+1] != nums[i+1:i+2] and nums[i+1:i+2] != []):
-            if(len(nums) // 2 > count):
-                count += 1
-
-    return count
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
