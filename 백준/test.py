cef6876f062a2949a43f8e23292e503f46172647
cet4713@gmail.com <cet4713@gmail.com>
2024-03-28 22:49:52 +0900

    파이썬 테스트 파일

---
+++ 백준/test.py
@@ -0,0 +1,17 @@
+import copy
+
+a = int(input())
+b = input()
+
+c = copy.b
+
+print(b)
+
+print(a * int(c[0]))
+print(a * int(c[1]))
+print(a * b)
+
+
+#입출력
+# data = list(map(int, input().split()))
+#a,b,c = map(int, input().split())
