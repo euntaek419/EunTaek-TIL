3cdf802604c0bb06994b8b531767541f8de16fa2
cet4713@gmail.com <cet4713@gmail.com>
2024-04-01 07:52:39 +0900

    백준 입력 출력 TestCase

--- 백준/test.py
+++ 백준/test.py
@@ -1,13 +1,18 @@
-
-
-#입출력
+# 입력
 # data = list(map(int, input().split()))
 # a,b,c = map(int, input().split())
 # a = int(input())
+
+# 빠른 입력
+# import sys
+# input = sys.stdin.readline().rstrip() #문자열 입력
     
-#T 입력받기
+# TestCase 입력
 # T = int(input()):
-# for i in range(T):
-#     A, B = map(int, input().split());
+# for i in range(T)
+#     A, B = map(int, input().split())
 #     result = A + B;
 #     print("Case")
+    
+# TestCase 출력
+# print("Case #{}: {} + {} = {}".format(A, B, C, D))
