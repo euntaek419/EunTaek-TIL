1f6ea9e5138b2f194caf5a193369e811b8c5f31c
cet4713@gmail.com <cet4713@gmail.com>
2024-04-02 22:37:04 +0900

    기본 코드 수정

--- 백준/test.py
+++ 백준/test.py
@@ -3,9 +3,10 @@
 # a,b,c = map(int, input().split())
 # a = int(input())
 
-# 빠른 입력
+# 빠른 문자열 입력
 # import sys
-# input = sys.stdin.readline.rstrip() #문자열 입력
+# input = sys.stdin.readline
+# input = sys.stdin.readline.rstrip()
 
 # TestCase 입력
 # T = int(input())
