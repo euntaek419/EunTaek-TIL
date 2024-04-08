978e04019861139680d46ce19c3548b3b1441041
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2024-04-09 06:51:53 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,8 @@
+word = list(input())
+count = 0
+result = {'A':2, 'B':2, 'C':2, 'D':3, 'E':3, 'F':3, 'G':4, 'H':4, 'I':4, 'J':5, 'K':5, 'L':5, 'M':6, 'N':6, 'O':6, 'P':7, 'Q':7, 'R':7, 'S':7, 'T':8, 'U':8, 'V':8, 'W':9, 'X':9, 'Y':9, 'Z':9}
+
+for i in range(0, len(word)):
+    count = count + result[word[i]]
+
+print(count+len(word))
