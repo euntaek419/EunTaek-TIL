ce29c166931a81589b95e752792d479ce61e2748
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2024-05-11 07:23:53 +0900

    [level 1] Title: 문자열 나누기, Time: 1.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,27 +1,18 @@
 def solution(s):
     s = list(s)
     word = ''
-    temp = ''
     
-    result = []
-    
-    count = 0
+    count = result = 0
     
     for i in range(0, len(s)):
         if(count == 0):
-            result.append(temp)
-            temp = ''
             word = s[i]
+            result += 1
         
         if(s[i] == word):
-            temp = temp + s[i]
             count += 1
             
         elif(s[i] != word):
             count -= 1
-            temp = temp + s[i]
-    
-    return len(result)
-            
-        
-        
+
+    return result
