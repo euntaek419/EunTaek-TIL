14552561ffbf999e114a1dac3b879af0aa9b39d9
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2024-05-12 19:32:47 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -5,32 +5,39 @@ def solution(n, lost, reserve):
     lost = sorted(lost)
     reserve = sorted(reserve)
     
-    #모두 가지고 있다고 가정
-    for i in range(0, n):
-        result.append(1)
+    new_lost = []
+    new_reserve = []
     
-    #여분 있는 사람 찾아내기
-    for i in reserve:
-        result[i-1] += 1
-        
-    #잃어버린 사람 추려내기
     for i in lost:
-        result[i-1] -= 1
+        if i not in reserve:
+            new_lost.append(i)
+    
+    for i in reserve:
+        if i not in lost:
+            new_reserve.append(i)
                 
     
-    #빌려줄 수 있으면 앞쪽 사람부터 빌려주기
-    for i in reserve:
-        if(i > 2 and result[i-2] == 0 and result[i-1] == 2): #2번째보다 크면 앞으로 빌려주기 가능
+    # 모두 가지고 있다고 가정
+    for i in range(0, n):
+        result.append(1)
+        
+    # 잃어버린 사람 추려내기
+    for i in new_lost:
+        result[i-1] -= 1
+        
+    for i in new_reserve:
+        result[i-1] += 1
+                
+    # 빌려줄 수 있으면 앞쪽 사람부터 빌려주기
+    for i in new_reserve:
+        if(i > 1 and result[i-2] == 0): # 2번째보다 크면 앞으로 빌려주기 가능
             result[i-2] += 1
-            result[i-1] -= 1
+            continue # 앞 학생에게 빌려주었으면 건너뛰기
 
-        elif(i < n and result[i] == 0 and result[i-1] == 2): # 또는 뒷 번호에 빌려주기
+        if(i < n and result[i] == 0): # 또는 뒷 번호에 빌려주기
             result[i] += 1
-            result[i-1] -= 1
-
-    print(result)
     
-    #세기
+    # 세기
     for i in result:
         if(i >= 1):
             count += 1
