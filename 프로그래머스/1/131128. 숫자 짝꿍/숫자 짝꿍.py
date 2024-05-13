cc7b047d1796b50005513f17cb52099813f408f6
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2024-05-13 18:33:05 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+def solution(X, Y):
+    X = Counter(X)
+    Y = Counter(Y)
+    
+    # 공통 숫자와 그 빈도를 찾는다
+    XY = X & Y
+    
+    # 공통 숫자들을 기반으로 결과 문자열을 만든다
+    result = []
+    for n1, n2  in XY.items():
+        result.extend([n1] * n2)
+    
+    # 결과 리스트를 내림차순으로 정렬
+    result.sort(reverse=True)
+    
+    # 결과 문자열이 비어있으면 -1을 반환
+    if not result:
+        return "-1"
+    
+    # 결과 문자열을 만든다
+    result = ''.join(result)
+    
+    # 결과가 0으로만 구성된 경우
+    if result[0] == '0':
+        return '0'
+    
+    return result
