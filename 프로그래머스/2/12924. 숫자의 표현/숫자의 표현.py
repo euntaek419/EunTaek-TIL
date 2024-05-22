4e3114dfd95d562cb25a34969c53b63f2dd12478
euntaek419 <100109284+euntaek419@users.noreply.github.com>
2024-05-23 03:54:05 +0900

    [level 2] Title: 숫자의 표현, Time: 3.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -3,11 +3,14 @@ def solution(n):
     count = 1
     
     for i in range(1, n//2+1):
-        t = 0
-        while t < n:
-            t += i
-            i += 1
-        if(t == n):
-            count += 1
-
+        temp = 0
+        for j in range(i, n):
+            temp += j
+            if(temp == n):
+                count += 1
+                break;
+            elif(temp > n):
+                break;
+            
     return count
+        
